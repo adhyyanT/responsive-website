905043e04908769467abb1002dcb8836ba3f021f
adhyyanT <adhyyan321@gmail.com>
2021-01-08 19:41:16 +0530

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import React from 'react';
+import Cards from '../Cards';
+
+export function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  );
+}
